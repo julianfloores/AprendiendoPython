577bf69a5dadc0f15909cc683df61e05e3298681
julianfloores <55817634+julianfloores@users.noreply.github.com>
2019-09-25 19:56:35 -0500

    Add files via upload

---
+++ Multiplo.py
@@ -0,0 +1,9 @@
+numero=int(input("Dame un numero entero: "))
+esMultiplo3=((numero%3)==0)
+esMultiplo5=((numero%5)==0)
+esMultiplo7=((numero%7)==0)
+
+if((esMultiplo3 and esMultiplo5)or esMultiplo7):
+    print("Correcto.")
+else:
+    print("Incorrecto.")
